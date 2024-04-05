aacd4ce7b8110456ac6ec20d348f4cb87a0e1b26
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-06 01:46:32 +0500

    Update eks_commands.sh

--- Adversarial AWS/Command Cheatsheets/eks_commands.sh
+++ Adversarial AWS/Command Cheatsheets/eks_commands.sh
@@ -8,3 +8,14 @@ aws eks delete-cluster --name <cluster-name>
 
 # List information about the cluster
 aws eks describe-cluster --name <cluster-name>
+
+# List all clusters in you region
+aws eks list-clusters
+
+# Tag a resource
+aws eks tag-resource --resource-arn <resource-arn> --tags <tags>
+
+# Untag a resource
+aws eks untag-resource --resource-arn <resource-arn> --tag-keys <tag-key>
+
+
