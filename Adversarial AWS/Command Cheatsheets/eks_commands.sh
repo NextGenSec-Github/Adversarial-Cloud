13e65580b1576fd00852a35001f5c3c8e5386d85
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-06 00:50:39 +0500

    Create eks_commands.sh

---
+++ Adversarial AWS/Command Cheatsheets/eks_commands.sh
@@ -0,0 +1,10 @@
+# GENERAL COMMANDS
+
+# Create a cluster
+aws eks create-cluster --name <cluster-name>
+
+# Delete a cluster 
+aws eks delete-cluster --name <cluster-name>
+
+# List information about the cluster
+aws eks describe-cluster --name <cluster-name>
